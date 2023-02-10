a1b7a46f476d7bde94217ca9c909090e91084a8b
Julian Bensch <bensch.julian@gmail.com>
2023-02-10 15:58:19 +0100

    Fixed enum generation (now without scan and value funcs)

--- internal/gen-go-model/gen/enum.go
+++ internal/gen-go-model/gen/enum.go
@@ -17,74 +17,18 @@ func (g *generator) genEnum(enum core.Enum) error {
 
 	f.Commentf("%s is generated type for enum '%s'", enumGoTypeName, enumOriginName)
 
-	f.Type().Id(enumGoTypeName).Int64()
+	f.Type().Id(enumGoTypeName).String()
 
 	f.Const().DefsFunc(func(group *jen.Group) {
-		group.Id("_").Id(enumGoTypeName).Op("=").Iota()
-
 		for _, value := range enum.Values {
-			v := group.Id(genutil.NormalLizeGoName(value.Name))
+			capsName := genutil.NormalLizeGoName(value.Name)
+			stringEncapsuledEnumValue := fmt.Sprintf("%q", value.Name)
+			v := group.Id(capsName).Id(enumGoTypeName).Op("=").Id(stringEncapsuledEnumValue)
 			if value.Note != "" {
 				v.Comment(value.Note)
 			}
 		}
 	})
-
-	f.Commentf("Scan implements Scan() from the sql.Scanner interface for %q", enumOriginName)
-	f.Commentf("See: https://pkg.go.dev/database/sql#Scanner")
-
-	f.Func().Params(
-		jen.Id("v").Op("*").Id(enumGoTypeName),
-	).Id("Scan").Params(
-		jen.Id("i").Interface(),
-	).Params(jen.Err().Error()).Block(
-		jen.List(jen.Id("s"), jen.Id("ok")).Op(":=").Id("i").Assert(jen.String()),
-		jen.If(jen.Op("!").Id("ok").Block(
-			jen.Return(jen.Qual("fmt", "Errorf").Call(jen.List(jen.Lit("value (%#v) is not of the expected type string"), jen.Id("i")))),
-		)),
-		jen.Switch().BlockFunc(func(group *jen.Group) {
-			for _, value := range enum.Values {
-				v := genutil.NormalLizeGoName(value.Name)
-
-				group.Case(jen.Qual("reflect", "DeepEqual").Call(jen.List(jen.Id("s"), jen.Lit(value.Name)))).Block(
-					jen.Op("*").Id("v").Op("=").Id(v),
-				)
-			}
-
-			group.Default().Block(
-				jen.Err().Op("=").Qual("fmt", "Errorf").Call(jen.List(jen.Lit(fmt.Sprintf("unable to parse %%#v into a known %s type", enumGoTypeName)), jen.Id("i"))),
-			)
-		}),
-		jen.Return(),
-	)
-
-	f.Commentf("Value implements Value() from the driver.Valuer interface for %q", enumOriginName)
-	f.Commentf("See: https://pkg.go.dev/database/sql/driver#Valuer")
-
-	if g.isPostgres {
-		f.Func().Params(
-			jen.Id("v").Id(enumGoTypeName),
-		).Id("Value").Params().Params(jen.Id("out").Qual("database/sql/driver", "Value"), jen.Id("err").Error()).Block(
-			jen.Switch(jen.Id("v")).BlockFunc(func(group *jen.Group) {
-				for idx, value := range enum.Values {
-					group.Case(jen.Lit(idx + 1)).Block(
-						jen.Id("out").Op("=").Lit(value.Name),
-					)
-				}
-
-				group.Default().Block(
-					jen.Err().Op("=").Qual("fmt", "Errorf").Call(jen.List(jen.Lit(fmt.Sprintf("unable to parse %%#v into a known %s string", enumGoTypeName)), jen.Id("v"))),
-				)
-			}),
-			jen.Return(),
-		)
-	} else {
-		f.Func().Params(
-			jen.Id("v").Id(enumGoTypeName),
-		).Id("Value").Params().Params(jen.Qual("database/sql/driver", "Value"), jen.Error()).Block(
-			jen.Return(jen.List(jen.Int64().Params(jen.Id("v")), jen.Nil())),
-		)
-	}
 
 	f.Comment(g.headerComments())
 
